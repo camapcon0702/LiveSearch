0c0597f645614f532b75f79ffb6d0b95e00402a2
camapcon0702 <quyle070204@gmail.com>
2024-10-24 07:16:12 +0700

    Add project files.

---
+++ LiveSearch/SearchHandle.ashx
@@ -0,0 +1,49 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="SearchHandle.ashx.cs" Class="LiveSearch.SearchHandle" %>
+
+using System;
+using System.Web;
+using System.Xml;
+
+public class SearchHandler : IHttpHandler {
+
+    public void ProcessRequest (HttpContext context) {
+        string query = context.Request.QueryString["q"];
+        string response = "no suggestion";
+
+        if (!string.IsNullOrEmpty(query)) {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(context.Server.MapPath("~/Links.xml"));
+
+            XmlNodeList linkNodes = xmlDoc.GetElementsByTagName("link");
+            string hint = "";
+
+            foreach (XmlNode linkNode in linkNodes) {
+                XmlNode titleNode = linkNode["title"];
+                XmlNode urlNode = linkNode["url"];
+
+                if (titleNode != null && urlNode != null) {
+                    if (titleNode.InnerText.IndexOf(query, StringComparison.OrdinalIgnoreCase) >= 0) {
+                        if (string.IsNullOrEmpty(hint)) {
+                            hint = $"<a href='{urlNode.InnerText}' target='_blank'>{titleNode.InnerText}</a>";
+                        } else {
+                            hint += $"<br /><a href='{urlNode.InnerText}' target='_blank'>{titleNode.InnerText}</a>";
+                        }
+                    }
+                }
+            }
+
+            if (!string.IsNullOrEmpty(hint)) {
+                response = hint;
+            }
+        }
+
+        context.Response.ContentType = "text/html";
+        context.Response.Write(response);
+    }
+
+    public bool IsReusable {
+        get {
+            return false;
+        }
+    }
+}
---
+++ LiveSearch/SearchHandle.ashx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+
+namespace LiveSearch
+{
+    /// <summary>
+    /// Summary description for Handler1
+    /// </summary>
+    public class SearchHandle : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string query = context.Request.QueryString["q"];
+            string response = "no suggestion";
+
+            if (!string.IsNullOrEmpty(query))
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(context.Server.MapPath("~/Links.xml"));
+
+                XmlNodeList linkNodes = xmlDoc.GetElementsByTagName("link");
+                string hint = "";
+
+                foreach (XmlNode linkNode in linkNodes)
+                {
+                    XmlNode titleNode = linkNode["title"];
+                    XmlNode urlNode = linkNode["url"];
+
+                    if (titleNode != null && urlNode != null)
+                    {
+                        if (titleNode.InnerText.IndexOf(query, StringComparison.OrdinalIgnoreCase) >= 0)
+                        {
+                            if (string.IsNullOrEmpty(hint))
+                            {
+                                hint = $"<a href='{urlNode.InnerText}' target='_blank'>{titleNode.InnerText}</a>";
+                            }
+                            else
+                            {
+                                hint += $"<br /><a href='{urlNode.InnerText}' target='_blank'>{titleNode.InnerText}</a>";
+                            }
+                        }
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(hint))
+                {
+                    response = hint;
+                }
+            }
+
+            context.Response.ContentType = "text/html";
+            context.Response.Write(response);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
